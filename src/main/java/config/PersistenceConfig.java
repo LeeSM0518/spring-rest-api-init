7e1493f031ce5b3878be71e0d03953c4adb07546
sangminLee <nalsm98@naver.com>
2020-04-27 20:38:18 +0900

    data upload

---
+++ src/main/java/config/PersistenceConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("mapper")
+public class PersistenceConfig {
+
+  @Bean(destroyMethod = "close")
+  public DataSource dataSource() {
+    DataSource ds = new DataSource();
+    ds.setDriverClassName("");
+    ds.setUrl("");
+    ds.setPassword("");
+    ds.setInitialSize(2);
+    ds.setMaxActive(10);
+    return ds;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    DataSourceTransactionManager tm = new DataSourceTransactionManager();
+    tm.setDataSource(dataSource());
+    return tm;
+  }
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+    factoryBean.setDataSource(dataSource());
+    return factoryBean.getObject();
+  }
+
+}
